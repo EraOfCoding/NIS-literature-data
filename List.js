e010dfff671ab47de19aa8d0986e476b55aedf18
Yerassyl <63988030+EraOfCoding@users.noreply.github.com>
2021-02-23 18:16:53 +0600

    Add files via upload
    main file

---
+++ List.js
@@ -0,0 +1,433 @@
+const list = {
+    //q7q; q means grade's language, 7 means grade, q means subject(qazaq)
+    //kazakh grades
+    q7q: [
+        {
+            name: "Үйсін Төле би мен Әнет баба",
+            url: 'https://bilim-all.kz/article/9448-Uisin-Tole-bi-men-Anet-baba',
+        },
+        {
+            name: 'Байдалы мен Төле би',
+            url: 'https://bilim-all.kz/article/9449-Baidaly-men-Tole',
+        },
+        {
+            name: "Қазыбек бидің елшілігі",
+            url: 'https://bilim-all.kz/article/9451',
+        },
+        {
+            name: 'Кім жақын, не қымбат, не қиын?',
+            url: 'https://ummet.kz/makalalar/349-uly-tulgalar/6393-kim-zha-yn-ne-ymbat-ne-iyn.html',
+        },
+        {
+            name: "Әйтеке бидің сыншылдығы",
+            url: 'https://bilim-all.kz/article/9440-Aiteke-bidin-synshyldygy',
+        },
+        {
+            name: 'Қозы көрпеш Баян сұлу',
+            url: 'https://kitap.kz/book/qozy-korpesh-bayan-sulu',
+        },
+        {
+            name: "Көксерек",
+            url: 'https://kitap.kz/audio-book/kokserek',
+        },
+        {
+            name: 'Жүз жылдық жара',
+            url: 'https://kitap.kz/book/zhuz-zhyldyq-zhara',
+        },
+        {
+            name: 'Жусан иісі',
+            url: 'https://kitap.kz/book/zhusan-iisi',
+        },
+    ],
+    q7r: [
+        {
+            name: "Гуливер в стране лилипутов",
+            url: 'https://teremok.in/Pisateli/Zarub_Pisateli/svift/gulliver_liliput.htm',
+        },
+        {
+            name: 'Зима не даром злится',
+            url: 'https://www.culture.ru/poems/46382/zima-nedarom-zlitsya',
+        },
+        {
+            name: "Некрасивая девочка",
+            url: 'https://www.culture.ru/poems/39086/nekrasivaya-devochka',
+        },
+        {
+            name: 'Цветок кактуса',
+            url: 'http://www.florets.ru/tsvetochnye-skazki/tsvetok-kaktusa.html',
+        },
+        {
+            name: "Кавказкий пленник",
+            url: 'https://ilibrary.ru/text/1846/p.1/index.html',
+        },
+        {
+            name: 'Ночь перед Рождеством',
+            url: 'https://ilibrary.ru/text/1088/p.24/index.html',
+        },
+        {
+            name: "Сказка о потерянном времени",
+            url: 'http://books.rusf.ru/unzip/add-on/xussr_ty/shvare15.htm?1/2',
+        },
+        {
+            name: 'Слепой музыкант',
+            url: 'https://ilibrary.ru/text/2402/p.1/index.html',
+        },
+        {
+            name: "Старый повар",
+            url: 'http://paustovskiy-lit.ru/paustovskiy/text/rasskaz/staryj-povar.htm',
+        },
+        {
+            name: 'Фея музыки',
+            url: 'https://pritchi.ru/id_5472',
+        },
+        {
+            name: "Мальчик в тылу врага",
+            url: 'http://kaissenov.kz/index.php/ru/bibliografiya/rasskazy/274-nazvanie-rasskazy-29',
+        },
+        {
+            name: 'Варварство',
+            url: 'https://rustih.ru/musa-dzhalil-varvarstvo/',
+        },
+        {
+            name: 'Журавли',
+            url: 'https://www.culture.ru/poems/42290/zhuravli',
+        },
+    ],
+    q8q: [
+        {
+            name: 'Қалқаман мамыр',
+            url: 'https://kitap.kz/book/qalqaman-mamyr',
+        },
+        {
+            name: 'Батыр баян',
+            url: 'https://massaget.kz/okushyilarga/uy_tapsyirmasyi/58343/',
+        },
+        {
+            name: 'Найман ана ',
+            url: 'https://www.6alash.kz/kz/article/shingis-aytmatov-nayman-ana-angime.html#gsc.tab=0',
+        },
+        {
+            name: 'Аққулар ұйқтағанда',
+            url: 'https://bilim-all.kz/olen/1557',
+        },
+        {
+            name: 'Қаздар қайтып барады',
+            url: 'https://kitap.kz/book/qabdesh-zhumadilov-qazdar-qajtyp-barady',
+        },
+        {
+            name: 'Ана, Аруана батыр дүние',
+            url: 'https://kitap.kz/audio-book/olengderi-14',
+        },
+        {
+            name: 'Кәрі қыран',
+            url: 'https://bilim-all.kz/olen/9704-Kari-qyran',
+        },
+        {
+            name: 'Бабамыздың шоқ басқан табанымен',
+            url: 'https://massaget.kz/blogs/13417/',
+        },
+    ],
+    q8r: [
+        {
+            name: 'Когда я был маленьким',
+            url: 'https://librebook.me/kogda_ia_byl_malenkim/vol1/1',
+        },
+        {
+            name: 'Детство Марселя',
+            url: 'https://modernlib.net/books/panol_marsel/detstvo_marselya/read/',
+        },
+        {
+            name: 'Событие',
+            url: 'https://ilibrary.ru/text/1369/p.1/index.html',
+        },
+        {
+            name: 'Ход белой королевы',
+            url: 'http://booksonline.com.ua/view.php?book=55395',
+        },
+        {
+            name: 'Уроки французкого',
+            url: 'http://www.serann.ru/text/uroki-frantsuzskogo-9306',
+        },
+        {
+            name: 'Песчаная учительница',
+            url: 'https://ilibrary.ru/text/1195/p.1/index.html',
+        },
+        {
+            name: 'О мастерах',
+            url: 'http://www.bards.ru/archives/part.php?id=54469',
+        },
+        {
+            name: 'Зелёное утро',
+            url: 'https://librebook.me/the_green_morning',
+        },
+        {
+            name: 'Гостья из будущего',
+            url: 'https://mybook.ru/author/kir-bulychev/gostya-iz-budushego/read/',
+        },
+        {
+            name: 'Рассказы о животных',
+            url: 'https://www.miloliza.com/rasskazy-o-zhivotnykh-seton-tompson',
+        },
+        {
+            name: 'Сын воды',
+            url: 'http://lib.ru/PRIKL/MAWR/waterson.txt',
+        },
+        {
+            name: 'Лодорский водопад',
+            url: 'https://stihi.ru/2011/04/02/9717',
+        },
+        {
+            name: 'Левитан',
+            url: 'https://reader.bookmate.com/JoE0zMXk',
+        },
+        {
+            name: '451 градус по Фаренгейту',
+            url: 'http://loveread.ec/view_global.php?id=2039',
+        },
+        {
+            name: 'Приключения электроника',
+            url: 'https://librebook.me/vse_prikliucheniia_elektronika',
+        },
+    ],
+    q9q: [
+        {
+            name: "Қыз жібек операсы",
+            url: 'https://kitap.kz/audio-book/qyz-zhibek-sejfullin',
+        },
+        {
+            name: "Зар заман",
+            url: 'https://bilim-all.kz/olen/6183-Zar-zaman',
+        },
+        {
+            name: 'Бала зары',
+            url: 'https://kitap.kz/book/kissa-shortanbay-kundy-kitap',
+        },
+        {
+            name: "Сабырсыз, арсыз, еріншек",
+            url: 'https://bilim-all.kz/olen/541-Sabyrsyz-arsyz-erinshek',
+        },
+        {
+            name: 'Мен жазбаймын өлеңді ермек үшін',
+            url: 'https://bilim-all.kz/olen/531-Men-zhazbaimyn-olendi-ermek-ushin',
+        },
+        {
+            name: "Малға достың мұны жоқмалдан басқа",
+            url: 'https://bilim-all.kz/olen/529-Malga-dostyn-muny-zhoq-maldan-basqa',
+        },
+        {
+            name: 'Сегіз аяқ',
+            url: 'https://bilim-all.kz/olen/544-Segiz-ayaq',
+        },
+        {
+            name: "Ескендір поемасы",
+            url: 'https://abai.kz/post/12967',
+        },
+        {
+            name: 'Қаһар романы',
+            url: 'https://kitap.kz/audio-book/koshpendiler-iii-kitap',
+        },
+        {
+            name: "Хан кене драммасы",
+            url: 'https://kitap.kz/book/xan-kene',
+        },
+        {
+            name: 'Ұлпан романы',
+            url: 'https://kitap.kz/book/ulpan',
+        },
+        {
+            name: "Әйел қырық жанды",
+            url: 'https://kitap.kz/book/ajel-qyryq-zhandy',
+        },
+    ],
+    q9r: [
+        {
+            name: "Мцыри",
+            url: 'https://ilibrary.ru/text/991/p.1/index.html',
+        },
+        {
+            name: 'Сказки',
+            url: 'https://ilibrary.ru/author/saltykov-schedrin/form.13/index.html',
+        },
+        {
+            name: "После бала",
+            url: 'https://ilibrary.ru/text/1005/p.1/index.html',
+        },
+        {
+            name: 'Собачье сердце',
+            url: 'http://rushist.com/index.php/rus-literature/3084-bulgakov-sobache-serdtse-polnyj-tekst',
+        },
+        {
+            name: "Судьба человека",
+            url: 'http://loveread.ec/view_global.php?id=18384',
+        },
+        {
+            name: 'Бегущая по волнам',
+            url: 'http://loveread.ec/view_global.php?id=18135',
+        },
+        {
+            name: "Алые паруса",
+            url: 'https://www.litres.ru/aleksandr-grin/alye-parusa-171957/chitat-onlayn/',
+        },
+        {
+            name: 'Собор Парижской богоматери',
+            url: 'http://loveread.ec/view_global.php?id=7631',
+        },
+        {
+            name: "Портрет Дориана Грея",
+            url: 'http://loveread.ec/contents.php?id=2502',
+        },
+        {
+            name: 'Граф Монте-Кристо',
+            url: 'http://loveread.ec/view_global.php?id=1759',
+        },
+        {
+            name: "В списках не значился",
+            url: 'http://loveread.ec/view_global.php?id=82440',
+        },
+        {
+            name: 'Завтра была война',
+            url: 'http://loveread.ec/view_global.php?id=1685',
+        },
+        {
+            name: "Живи и помни",
+            url: 'http://lib.ru/%3E%3C/PROZA/RASPUTIN/rasputin_zhivi.txt',
+        },
+        {
+            name: 'Белый пароход',
+            url: 'http://lib.ru/PROZA/AJTMATOW/parohod.txt',
+        },
+        {
+            name: "Первый учитель",
+            url: 'https://mybook.ru/author/chingiz-ajtmatov/pervyj-uchitel/read/',
+        },
+        {
+            name: 'Ночевала тучка золотая',
+            url: 'http://loveread.ec/view_global.php?id=19858',
+        },
+    ],
+    //russian grades
+    r7r: [
+        {
+            name: "Зеленое утро",
+            url: 'https://librebook.me/the_green_morning',
+        },
+        {
+            name: 'Повесть о том как один мужик двух генералов покормил',
+            url: 'https://ru.wikisource.org/wiki/%D0%9F%D0%BE%D0%B2%D0%B5%D1%81%D1%82%D1%8C_%D0%BE_%D1%82%D0%BE%D0%BC,_%D0%BA%D0%B0%D0%BA_%D0%BE%D0%B4%D0%B8%D0%BD_%D0%BC%D1%83%D0%B6%D0%B8%D0%BA_%D0%B4%D0%B2%D1%83%D1%85_%D0%B3%D0%B5%D0%BD%D0%B5%D1%80%D0%B0%D0%BB%D0%BE%D0%B2_%D0%BF%D1%80%D0%BE%D0%BA%D0%BE%D1%80%D0%BC%D0%B8%D0%BB_(%D0%A1%D0%B0%D0%BB%D1%82%D1%8B%D0%BA%D0%BE%D0%B2-%D0%A9%D0%B5%D0%B4%D1%80%D0%B8%D0%BD)',
+        },
+        {
+            name: "Левша",
+            url: 'https://ilibrary.ru/text/986/index.html',
+        },
+        {
+            name: 'Чайка по имени Джонатан Ливингстон',
+            url: 'http://www.litra.ru/fullwork/get/woid/00022401189507197281/',
+        },
+        {
+            name: "Рождество",
+            url: 'http://www.lib.ru/NABOKOW/rozhdestvo.txt',
+        },
+        {
+            name: 'Хлеб для собаки',
+            url: 'https://modernlib.net/books/tendryakov_vladimir_fedorovich/hleb_dlya_sobaki/read/',
+        },
+        {
+            name: 'Хоббит, или туда и обратно',
+            url: 'http://loveread.ec/view_global.php?id=2344',
+        }
+    ],
+    r8r: [
+        {
+            name: "Хамелеон",
+            url: 'https://ilibrary.ru/text/463/p.1/index.html',
+        },
+        {
+            name: 'Лошадинная фамилия',
+            url: 'https://ilibrary.ru/text/988/p.1/index.html',
+        },
+        {
+            name: "Недоросль",
+            url: 'https://ilibrary.ru/text/1098/p.1/index.html',
+        },
+        {
+            name: 'Ромео и Джулиетта',
+            url: 'http://loveread.ec/view_global.php?id=2872',
+        },
+        {
+            name: "Капитанская дочка",
+            url: 'https://ilibrary.ru/text/107/index.html',
+        },
+    ],
+    r9r: [
+        {
+            name: "Цыганы",
+            url: 'https://ilibrary.ru/text/445/p.1/index.html',
+        },
+        {
+            name: 'Ася',
+            url: 'https://ilibrary.ru/text/1097/index.html',
+        },
+        {
+            name: 'Крыжовник',
+            url: 'https://ilibrary.ru/text/460/p.1/index.html',
+        },
+        {
+            name: "Анна на шее",
+            url: 'https://ilibrary.ru/text/452/p.1/index.html',
+        },
+        {
+            name: 'Человек в футляре',
+            url: 'https://ilibrary.ru/text/438/p.1/index.html',
+        },
+        {
+            name: "Ионыч",
+            url: 'https://ilibrary.ru/text/437/p.1/index.html',
+        },
+        {
+            name: 'Ожерелье',
+            url: 'http://ocr.krossw.ru/html/mopassan/mopassan-ogerelie-ls_1.htm',
+        },
+        {
+            name: "Гранатовый браслет",
+            url: 'https://ilibrary.ru/text/1022/p.1/index.html',
+        },
+        {
+            name: 'Мцыри',
+            url: 'https://ilibrary.ru/text/991/p.1/index.html',
+        },
+        {
+            name: "Утес",
+            url: 'https://rustih.ru/mixail-lermontov-utyos/',
+        },
+        {
+            name: "Шекспир. Сонеты",
+            url: 'https://rustih.ru/shekspir/',
+        },
+        {
+            name: 'Ревизор',
+            url: 'https://ilibrary.ru/text/473/p.1/index.html',
+        },
+        {
+            name: "Аристократка",
+            url: 'http://www.litra.ru/fullwork/get/woid/00219261203324741642/',
+        },
+        {
+            name: 'Галоша',
+            url: 'https://nukadeti.ru/skazki/zoshhenko-galosha',
+        },
+        {
+            name: "Прозаседавшие",
+            url: 'https://ilibrary.ru/text/2388/p.1/index.html',
+        },
+        {
+            name: 'О дряни',
+            url: 'https://ilibrary.ru/text/2382/p.1/index.html',
+        },
+        {
+            name: "Безумная Евдокия",
+            url: 'http://lib.ru/PROZA/ALEKSIN/ewdokia.txt',
+        },
+    ],
+};
+
+export default list;
